c349a8050aabb8bd9d3b35d62f154f8d772faa1a
Kalinina Oksana <ksan4ik92@gmail.com>
2024-07-30 08:32:41 +0300

    feat: add images for puzzle on screen

--- src/screens/Screen3/Screen3.tsx
+++ src/screens/Screen3/Screen3.tsx
@@ -1,11 +1,26 @@
 import style from "./Screen3.module.css"
 import urlContur from "../../assets/images/border-puzzle.svg"
+import urlImg1 from "../../assets/images/puzzle/1-answer.png"
+import urlImg2 from "../../assets/images/puzzle/2-answer.png"
+import urlImg3 from "../../assets/images/puzzle/3-answer.png"
+import urlImg4 from "../../assets/images/puzzle/4-answer.png"
 interface IProps {
     changeScreen: () => void;
 }
 
 function Screen3(props: IProps) {
     const { changeScreen } = props;
+
+
+
+    const dataPuzzleClass = [style.puzzlePoz0, style.puzzlePoz1, style.puzzlePoz2, style.puzzlePoz3].sort(function () { return 0.5 - Math.random() })
+    console.log(dataPuzzleClass);
+
+
+
+    const dataImages = [
+        urlImg1, urlImg2, urlImg3, urlImg4
+    ]
     changeScreen()
     return (
         <div className={style.wrapper}>
@@ -14,6 +29,10 @@ function Screen3(props: IProps) {
                 <div className={style.puzzle}>
                     <div className={style.puzzle__contur}>
                         <img src={urlContur} alt="contur" />
+                        {dataImages.map((item, index) => <div className={`${style.puzzle__item} ${style[`puzzle${index}`]} ${dataPuzzleClass[index]}`}>
+                            <img src={item} alt={`puzzle${index}`} />
+                        </div>)}
+
                     </div>
 
                 </div>
